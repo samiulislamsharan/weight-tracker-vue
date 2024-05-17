49808f401c71a70256d728cae85bcacd9e843320
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-17 11:24:11 +0600

    feat: add project files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from 'vue'
+import Chart from 'chart.js/auto'
+
+const weights = ref([])
+
+const weighChartE1 = ref(null)
+
+const weightCart = shallowRef(null)
+
+const weightInput = ref(60.0)
+
+const currentWeight = computed(() => {
+  return weight.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+})
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date().getTime()
+  })
+}
+</script>
+
+<template>
+  <h1>Hello Vue</h1>
+</template>
+
+<style></style>
